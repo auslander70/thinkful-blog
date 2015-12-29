e6f9cb21ec33300e6d285ef8d67ac91b5a84d7b6
Wil DAmato <wil.damato@gmail.com>
2015-12-29 16:31:24 +0000

    add_entry form handler

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask import request, redirect, url_for
 
 from blog import app
 from .database import session, Entry 
@@ -35,3 +36,15 @@ def entries(page=1):
 @app.route("/entry/add", methods=["GET"])
 def add_entry_get():
   return render_template("add_entry.html")
+  
+@app.route("/entry/add", methods=["POST"])
+def add_entry_post():
+  entry = Entry(
+    title=request.form["title"],
+    content=request.form["content"],
+  )
+  session.add(entry)
+  session.commit()
+  return redirect(url_for("entries"))
+  
+  
